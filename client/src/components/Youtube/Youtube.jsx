6c1279e35456e6c4686e13e3f7d35bba15e4f428
HeyRupam <rupamb881@gmail.comm>
2023-11-23 00:06:24 +0530

    added basic css

--- client/src/components/Youtube/Youtube.jsx
+++ client/src/components/Youtube/Youtube.jsx
@@ -30,35 +30,39 @@ function Youtube() {
     e.preventDefault()
     setYtitag(index)
     console.log(ytitag);
-    axios.post('http://localhost:3001/download', {ytitag})
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
+    axios.post('http://localhost:3001/download', { ytitag })
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
   }
   return (
-    <div className='justify-content-center'>
-      <form onSubmit={handleSubmit}>
-        <div className='mb-3'>
-          <label htmlFor="yturl">Youtube URL</label>
-          <input id='yturl' type="url" name='yturl' placeholder='Enter your url' className='form-control rounded-0' onChange={(e) => setYturl(e.target.value)} required />
-        </div>
-        <button type='submit' className='btn btn-success w-100 rounded-0'>Submit</button>
-      </form>
-      <div>
-        {ytdetails ? (
-          <p>{ytdetails.title}</p>
-        ) : ''}
-      </div>
-      <div>
-        {ytformats && ytformats.length > 0 ? (
-          ytformats.map((format, index) => (
-            <div key={format.itag}>
-              <p>{format.qualityLabel}</p>
-              <button className='white rounded-0' onClick={(e) => handleDownload(e, index)}>Download Video</button>
+    <div className='container main_content'>
+      <div className=' row justify-content-center'>
+        <div className="col-lg-6">
+          <form onSubmit={handleSubmit}>
+            <div className='mb-3 text-center'>
+              <label className='mb-1' htmlFor="yturl">Youtube URL</label>
+              <input id='yturl' type="url" name='yturl' placeholder='Enter your url' className='form-control rounded-0' onChange={(e) => setYturl(e.target.value)} required />
             </div>
-          ))
-        ) : (
-          <p>No video available</p>
-        )}
+            <button type='submit' className='btn btn-success w-100 rounded-0'>Submit</button>
+          </form>
+          <div>
+            {ytdetails ? (
+              <p>{ytdetails.title}</p>
+            ) : ''}
+          </div>
+          <div>
+            {ytformats && ytformats.length > 0 ? (
+              ytformats.map((format, index) => (
+                <div key={format.itag}>
+                  <p>{format.qualityLabel}</p>
+                  <button className='white rounded-0' onClick={(e) => handleDownload(e, index)}>Download Video</button>
+                </div>
+              ))
+            ) : (
+              <p>No video available</p>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   )
